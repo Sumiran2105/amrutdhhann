5e5b01d3c51686df9bd98f8e4fbb67a142ed2c21
Sai Sumiran <164289250+Sumiran2105@users.noreply.github.com>
2025-12-21 03:00:51 +0530

    Use absolute imports to fix About page build

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,12 @@
-import Banner from "../components/about/Banner";
-import Vision from "../components/about/Vision";
-import Mission from "../components/about/Mission";
-import WhyAmrutdhhann from "../components/about/WhyAmrutdhhann";
-import OurJourney from "../components/about/OurJourney";
-import OurProducts from "../components/about/OurProducts";
-import AnimalWelfare from "../components/about/AnimalWelfare";
-import Investment from "../components/about/Investment";
-import Closing from "../components/about/Closing";
+import Banner from "@/components/about/Banner";
+import Vision from "@/components/about/Vision";
+import Mission from "@/components/about/Mission";
+import WhyAmrutdhhann from "@/components/about/WhyAmrutdhhann";
+import OurJourney from "@/components/about/OurJourney";
+import OurProducts from "@/components/about/OurProducts";
+import AnimalWelfare from "@/components/about/AnimalWelfare";
+import Investment from "@/components/about/Investment";
+import Closing from "@/components/about/Closing";
 
 
 const About = () => {
